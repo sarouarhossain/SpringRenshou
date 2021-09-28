004a59be3bf3d9fea32c3f723680929b513175d2
Hossain, Sarouar | Sarouar | ADPDE <sarouar.hossain@rakuten.com>
2021-09-28 19:21:48 +0900

    first commit

---
+++ src/main/java/com/sarouar/renshuo/RenshuoApplication.java
@@ -0,0 +1,13 @@
+package com.sarouar.renshuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenshuoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenshuoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarouar/renshuo/RenshuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarouar.renshuo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenshuoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
